4974898218c3772ffd3f15e36a2ea50eda1ca3ce
Dev <85393786+devanshs-ai@users.noreply.github.com>
2026-08-23 20:40:03 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int left = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[left-1]!=nums[i]){
+                nums[left] = nums[i];
+                left++;
+            }
+        }
+
+        return left; 
+    }
+};
